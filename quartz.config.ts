3c48018f59b2e0c91f3669cd484fcae3dbfdd760
masaki39 <masa21gifus@gmail.com>
2024-02-09 00:37:59 +0900

    Quartz sync: Feb 9, 2024, 12:37 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "砂の書庫",
+    pageTitle: "⏳砂の書庫⌛",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
